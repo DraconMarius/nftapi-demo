bbdc73dcf562121dbc5f106e6d1206cc87a78630
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2024-03-15 18:40:46 -0400

    basic server up

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`))
